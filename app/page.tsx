479e27335daaf3ccbbf6653de4d1241559888f34
Bijou <bijou.raj12@gmail.com>
2025-08-25 15:49:55 -0700

    Fixed cache

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,8 @@ import { BlogPostCard } from "@/components/general/BlogpostCard";
 import { Suspense } from "react";
 import { Skeleton } from "@/components/ui/skeleton";
 
+export const revalidate = 60;
+
 async function getData() {
   const data = await prisma.blogPost.findMany({
     select: {
